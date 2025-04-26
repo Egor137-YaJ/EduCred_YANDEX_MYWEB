c2c2bed78d2fbd3675a105e0709ff7758f90da6f
EgorHomePCkld <egorsid777@icloud.com>
2025-04-27 00:17:10 +0200

    making-forms-2---made registration form(flask_wtf) Student

---
+++ data/reg_Student.py
@@ -0,0 +1,21 @@
+from flask_wtf import FlaskForm
+from wtforms import SubmitField, PasswordField, StringField, DateField
+from wtforms.fields import EmailField
+from wtforms.validators import DataRequired, Regexp
+
+
+class RegisterStudentForm(FlaskForm):
+    NSP = StringField('ФИО(через пробел)', validators=[DataRequired()])
+    born_date = DateField('Дата рождения (дд.мм.гггг)',
+                           format='%d.%m.%Y',
+                           validators=[DataRequired()])
+    phone_number = StringField('Номер телефона',
+                        validators=[
+                            DataRequired(),
+                            Regexp(r'^(?:\+7|8)\(?\d{3}\)?[-.\s]?\d{3}[-.\s]?\d{2}[-.\s]?\d{2}$')
+                        ])
+    about = StringField('О себе', validators=[DataRequired()])
+    email = EmailField('Почта', validators=[DataRequired()])
+    password = PasswordField('Пароль', validators=[DataRequired()])
+    password_again = PasswordField('Повторите пароль', validators=[DataRequired()])
+    submit = SubmitField('Подтвердить')

23d7a9b98557e976882c8663f0cfb5e7bfb7052c
Smart developer <kostya.vit.99@mail.ru>
2025-04-28 21:47:26 +0200

    start working with homepage

--- server.py
+++ server.py
@@ -25,67 +25,67 @@ app.config["SECRET_KEY"] = "yandexlyceum_secret_key"
 
 
 
-@app.route('/home')
+
 @app.route('/')
 def home():
     return render_template('home.html', title='Home Page')
 
 
 @app.route('/register_university', methods=['GET', 'POST'])
-def reqister_university():
+def register_university():
     form = RegisterUniverForm()
-    if form.validate_on_submit():
-        if form.password.data != form.password_again.data:
-            return render_template('register_university.html', title='University Registration',
-                                   form=form, style=url_for('static', filename='css/style.css'),
-                                   message="Passwords don't match")
-        db_sess = db_session.create_session()
-
-        if db_sess.query(University).filter(University.INN == form.INN.data).first():
-            return render_template('register_university.html', title='University Registration',
-                                   form=form, style=url_for('static', filename='css/style.css'),
-                                   message="This University already exists")
-        if db_sess.query(User).filter(User.email == form.email.data).first():
-            return render_template('register_university.html', title='University Registration',
-                                   form=form, style=url_for('static', filename='css/style.css'),
-                                   message="Account with this email already exists")
-        if any(user.check_password(form.password.data) for user in db_sess.query(User).all()):
-            return render_template('register_university.html', title='University Registration',
-                                   form=form, style=url_for('static', filename='css/style.css'),
-                                   message="Account with this password already exists")
-
-        title, address, boss_nsp = get_university_info_by_inn(form.INN.data)
-        type = dict(form.type.choices).get(form.type.data)
-
-        if 'Ошибка' in ' '.join([title, address, boss_nsp]):
-            return render_template('register_university.html', title='University Registration',
-                                   form=form, style=url_for('static', filename='css/style.css'),
-                                   message="Error in INN request - may be non existing INN")
-
-        if type.lower() not in title.lower():
-            return render_template('register_university.html', title='University Registration',
-                                   form=form, style=url_for('static', filename='css/style.css'),
-                                   message="Types doesn't match")
-
-        user = User(
-            email=form.email.data,
-            role='university'
-        )
-        user.set_password(form.password.data)
-        db_sess.add(user)
-        db_sess.commit()
-
-        univer = University(
-            user_id=user.id,
-            INN=form.INN.data,
-            title=title,
-            address=address,
-            boss_nsp=boss_nsp,
-            type=type
-        )
-        db_sess.add(univer)
-        db_sess.commit()
-        return redirect('/home')
+    # if form.validate_on_submit():
+    #     if form.password.data != form.password_again.data:
+    #         return render_template('register_university.html', title='University Registration',
+    #                                form=form, style=url_for('static', filename='css/style.css'),
+    #                                message="Passwords don't match")
+    #     db_sess = db_session.create_session()
+    #
+    #     if db_sess.query(University).filter(University.INN == form.INN.data).first():
+    #         return render_template('register_university.html', title='University Registration',
+    #                                form=form, style=url_for('static', filename='css/style.css'),
+    #                                message="This University already exists")
+    #     if db_sess.query(User).filter(User.email == form.email.data).first():
+    #         return render_template('register_university.html', title='University Registration',
+    #                                form=form, style=url_for('static', filename='css/style.css'),
+    #                                message="Account with this email already exists")
+    #     if any(user.check_password(form.password.data) for user in db_sess.query(User).all()):
+    #         return render_template('register_university.html', title='University Registration',
+    #                                form=form, style=url_for('static', filename='css/style.css'),
+    #                                message="Account with this password already exists")
+    #
+    #     title, address, boss_nsp = get_university_info_by_inn(form.INN.data)
+    #     type = dict(form.type.choices).get(form.type.data)
+    #
+    #     if 'Ошибка' in ' '.join([title, address, boss_nsp]):
+    #         return render_template('register_university.html', title='University Registration',
+    #                                form=form, style=url_for('static', filename='css/style.css'),
+    #                                message="Error in INN request - may be non existing INN")
+    #
+    #     if type.lower() not in title.lower():
+    #         return render_template('register_university.html', title='University Registration',
+    #                                form=form, style=url_for('static', filename='css/style.css'),
+    #                                message="Types doesn't match")
+    #
+    #     user = User(
+    #         email=form.email.data,
+    #         role='university'
+    #     )
+    #     user.set_password(form.password.data)
+    #     db_sess.add(user)
+    #     db_sess.commit()
+    #
+    #     univer = University(
+    #         user_id=user.id,
+    #         INN=form.INN.data,
+    #         title=title,
+    #         address=address,
+    #         boss_nsp=boss_nsp,
+    #         type=type
+    #     )
+    #     db_sess.add(univer)
+    #     db_sess.commit()
+    #     return redirect('/home')
     return render_template('register_university.html', title='University Registration',
                            form=form, style=url_for('static', filename='css/style.css'))
 
@@ -94,7 +94,7 @@ def reqister_university():
 
 
 def main():
-    db_session.global_init("db/EduCred_data.db")
+    # db_session.global_init("db/EduCred_data.db")
     # port = int(os.environ.get("PORT", 8000))
     # app.run(host='0.0.0.0', port=port)
     app.run()

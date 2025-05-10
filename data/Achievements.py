e34c5fc43e6d4f41568e396427b0a26495a89179
Throddy <maxim2007volkov@yandex.ru>
2025-05-10 22:24:56 +0200

    home certificate check done

--- data/Achievements.py
+++ data/Achievements.py
@@ -12,6 +12,7 @@ class Achievement(SqlAlchemyBase, UserMixin, SerializerMixin):
 
     id = sqlalchemy.Column(sqlalchemy.Integer, primary_key=True, autoincrement=True)
     token = sqlalchemy.Column(sqlalchemy.String(64), nullable=False, unique=True, default=lambda: str(uuid.uuid4()))
+    approve_path = sqlalchemy.Column(sqlalchemy.String, nullable=True)
     file_path = sqlalchemy.Column(sqlalchemy.String, nullable=False)
     title = sqlalchemy.Column(sqlalchemy.String, nullable=False)
     start_date = sqlalchemy.Column(sqlalchemy.DateTime, nullable=False, default=datetime.datetime.now())
